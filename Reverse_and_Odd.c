4906a416319c3d3550938c57041629a77b599fd5
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-04-19 20:55:04 +0600

    Update Reverse_and_Odd.c

--- Reverse_and_Odd.c
+++ Reverse_and_Odd.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main() {
+int main()
+{
 
     int n, i;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arry[n];
-    for ( i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        scanf ("%d",&arry[i]);
-        if (i%2==1)
+        scanf("%d", &arry[i]);
+    }
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (i % 2 == 1)
         {
-            for ( i = n-1; i >=0; i--)
-            {
-                printf("%d",arry[i]);
-            }
-            
+
+            printf("%d ", arry[i]);
         }
-        
     }
 
     return 0;
