72b6fd393a0bf7406409bc9bc4c48c066eb2f732
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-04-26 23:04:57 +0600

    Add files via upload

---
+++ deta stractur/28 apr/2.cpp
@@ -0,0 +1,18 @@
+/*
+    MD Abdullah al moneem
+
+    Institute:- BAIUST
+    Country:- BANGLADESH
+    date: 2024-04-26 22:40:54
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+    return 0;
+}
