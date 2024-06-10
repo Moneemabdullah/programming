beed13d140840dc68575eca069d05632728e0e4e
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-06-11 00:08:59 +0600

    Update ct_2_all_set_Q_2_ans.cpp

--- team/ct_2_all_set_Q_2_ans.cpp
+++ team/ct_2_all_set_Q_2_ans.cpp
@@ -4,6 +4,14 @@
     Institute:- BAIUST
     Country:- BANGLADESH
     date: 2024-05-19 21:03:13
+
+    CT 2 set 1:
+    design a class with privet member and declare a friend function outside the class to access that privet member 
+    show how the friend function manipulte the privet deta
+
+    CT 2 set 2:
+    crteate a fanction that takes an obj of a class as an argument ans performs some operation on it 
+    demostret passing obj by value and by reference,
 */
 
 #include <bits/stdc++.h>
