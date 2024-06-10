fd3539b27107fe89cfce9f9feff0c5e771c67615
Abdullah Al Moneem <92563775+Moneemabdullah@users.noreply.github.com>
2024-06-11 00:04:34 +0600

    Update ct_2_all_set_Q_1.cpp

--- team/ct_2_all_set_Q_1.cpp
+++ team/ct_2_all_set_Q_1.cpp
@@ -3,6 +3,16 @@
 
     Institute:- BAIUST
     Country:- BANGLADESH
+
+    CT 2 set 1
+    write a fanction to demostrate fanction overloading in a class,
+create multiple fanmction with same name but defferent perameter ans show 
+how they invoke
+
+    CT 2 set 2
+    Create a class with member fanction that takes defult arguments.
+    demostret caslling this fanction with and without passing arguments 
+    
 */
 
 #include <bits/stdc++.h>
